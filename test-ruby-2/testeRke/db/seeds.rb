85c4760540faeae53215c69be12863815a828d4b
Cayque Cicarelli <cayquecicarelli@gmail.com>
2022-09-30 12:16:40 -0300

    Adição do seed e gem Faker

--- test-ruby-2/testeRke/db/seeds.rb
+++ test-ruby-2/testeRke/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+    user_name = Faker::Name.name
+    user_email = Faker::Internet.email
+    user = "#{user_name} - #{user_email}"
+    puts "Cadastrando o #{user}"
+    User.create(name: user_name, email: user_email)
+end
